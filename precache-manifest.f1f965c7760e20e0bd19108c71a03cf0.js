1575f347c0e4d28a2253485a650e85ab6c60be24
Artur <medonchak999@gmail.com>
2020-09-05 20:25:41 +0300

    Updates

---
+++ precache-manifest.f1f965c7760e20e0bd19108c71a03cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc55d624a435133799c242971ae281e",
+    "url": "/medonchak/map/index.html"
+  },
+  {
+    "revision": "0f4f01ef62bfb4ead4c1",
+    "url": "/medonchak/map/static/css/main.e721a452.chunk.css"
+  },
+  {
+    "revision": "a55e3c676d4e4f0a2e67",
+    "url": "/medonchak/map/static/js/2.9f35f00e.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/medonchak/map/static/js/2.9f35f00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4f01ef62bfb4ead4c1",
+    "url": "/medonchak/map/static/js/main.23cb8d14.chunk.js"
+  },
+  {
+    "revision": "7d9019edd1d09cce31ee",
+    "url": "/medonchak/map/static/js/runtime-main.848e42eb.js"
+  }
+]);
